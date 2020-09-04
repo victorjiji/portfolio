a30605cdd699c2289f5df2b0f3a9c70bead68d61
Victor Jiji <trkvski@gmail.com>
2020-09-04 11:15:31 +0300

    Remade blogs app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# class Blog(models.Model):
+#     title = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField(null=True)
+#     body = models.TextField(default='')
+#     image = models.ImageField(upload_to='images/')
